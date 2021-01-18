baf362313893f2ba27a87f21f7554636f0d2cf31
iamblessed <idrisnau@gmail.com>
2021-01-18 13:15:58 +0200

    Backend CRUD completed

--- 1_devSetup/client/HelloWorld.js
+++ 1_devSetup/client/HelloWorld.js
@@ -8,6 +8,7 @@ const HelloWorld = () => {
 			<h2>Full Stack Development</h2>
 			<p>React is very simple and easy to learn.</p>
 			<h3>Success is mine in this world.</h3>
+			<p>In the end, I shall have the course to glorify his name.</p>
         </div>
       )
 }
---
+++ 2_crudBasic/index.js
@@ -0,0 +1,12 @@
+export default () => {
+	return `<!doctype html>
+		<html lang="en">
+			<head>
+			<meta charset="utf-8">
+				<title>Basic MERN App</title>
+			</head>
+			<body>
+				<div id="root">Explore my coding Tutorial</div>
+			</body>
+		</html>`
+}
